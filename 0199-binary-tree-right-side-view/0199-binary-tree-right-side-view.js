d0df6594655eca516fc18801e7041b002a5ac8ed
Christopher Wong <dontgotowardsthelight@gmail.com>
2023-05-31 19:21:27 -1000

    Time: 57 ms (83.43%), Space: 43.5 MB (76.74%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+      if (!root) {
+    return [];
+  }
+
+  const queue = [root];
+  const result = [];
+
+  while (queue.length > 0) {
+    const levelSize = queue.length;
+
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift();
+
+      // Only add the rightmost node of each level to the result
+      if (i === levelSize - 1) {
+        result.push(node.val);
+      }
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+  }
+
+  return result;
+};
